673c147ec6089ca26c33a082a9c4335f60c3c65d
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2024-05-28 21:38:15 -0300

    Alterações no start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -7,8 +7,14 @@ fi
 # Ativar o ambiente virtual
 source venv/bin/activate
 
+# Atualizar pip
+pip install --upgrade pip
+
+# Instalar as dependências
+pip install -r requirements.txt
+
 # Instalar Gunicorn se não estiver instalado
 pip install gunicorn
 
 # Iniciar o servidor Flask usando Gunicorn
-exec gunicorn --bind 0.0.0.0:5000 backend.app:app
+exec gunicorn --bind 0.0.0.0:5000 app:app
