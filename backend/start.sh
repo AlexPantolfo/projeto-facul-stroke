30afd6536238b48f720cd0d19f7b25f59d024fd8
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2024-05-28 21:13:16 -0300

    Alterando configs para deploy render

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# Criar o ambiente virtual se ainda não existir
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Ativar o ambiente virtual
 source venv/bin/activate
+
+# Instalar Gunicorn se não estiver instalado
+pip install gunicorn
+
+# Iniciar o servidor Flask usando Gunicorn
 exec gunicorn --bind 0.0.0.0:5000 app:app
