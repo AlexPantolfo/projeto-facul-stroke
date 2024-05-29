ac37cf35cf8b1f387a0b9f088d4d137f2050a012
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2024-05-29 01:57:58 -0300

    Melhorias no codigo

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn import metrics
@@ -18,7 +19,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})  # Permitir todas as origens
 # Load and preprocess the dataset
 avc = pd.read_csv('healthcare-dataset-stroke-data-teste.csv')
 avc = avc.drop(['id'], axis=1)
-avc['bmi'].fillna(avc['bmi'].mean(), inplace=True)
+avc['bmi'] = avc['bmi'].fillna(avc['bmi'].mean())
 avc = avc.replace({
     'gender': {'Male': 0, 'Female': 1, 'Other': 2},
     'ever_married': {'Yes': 0, 'No': 1},
@@ -31,7 +32,7 @@ avc = avc.replace({
 oversample = SMOTE(random_state=42)
 avc_x = avc.drop(['stroke'], axis=1)
 avc_y = avc['stroke']
-x_train_res, y_train_res = oversample.fit_resample(avc_x, avc_y.ravel())
+x_train_res, y_train_res = oversample.fit_resample(avc_x, avc_y.to_numpy())
 x_train, x_test, y_train, y_test = train_test_split(x_train_res, y_train_res, test_size=0.2, random_state=42)
 
 # Train models
@@ -92,6 +93,9 @@ def evaluate_models(x_test, y_test):
             result['ROC AUC'] = str(e)
 
         results.append(result)
+    
+    # Order results by Recall in descending order
+    results = sorted(results, key=lambda x: x['Recall'], reverse=True)
     return results
 
 @app.route('/')
@@ -102,8 +106,34 @@ def home():
 @cross_origin()
 def predict():
     try:
+        data = request.json
+        input_df = pd.DataFrame([data])
+        input_df = input_df.replace({
+            'gender': {'Male': 0, 'Female': 1, 'Other': 2},
+            'ever_married': {'Yes': 0, 'No': 1},
+            'work_type': {'Private': 0, 'Self-employed': 1, 'Govt_job': 2, 'children': 3, 'Never_worked': 4},
+            'smoking_status': {'formerly smoked': 0, 'never smoked': 1, 'smokes': 2, 'Unknown': 3},
+            'Residence_type': {'Urban': 0, 'Rural': 1}
+        }).infer_objects()
+
+        results = []
+        for name, model in models:
+            start_time = datetime.now()
+            y_pred = model.predict(input_df)
+            end_time = datetime.now()
+            execution_time = (end_time - start_time).total_seconds()
+
+            prediction = 'Sim' if y_pred[0] == 1 else 'Não'
+
+            results.append({
+                'Model': name,
+                'Prediction': prediction,
+                'Execution Time (s)': execution_time
+            })
+
         evaluation_results = evaluate_models(x_test, y_test)
         return jsonify({
+            'predictions': results,
             'evaluation': evaluation_results
         })
     except Exception as e:
@@ -111,4 +141,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
