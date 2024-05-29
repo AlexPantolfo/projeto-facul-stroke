b462435da7ea71dfc86a4df308a438690dfc1972
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2024-05-29 01:17:35 -0300

    Alterando start.sh para apenas executar o arquivo python

--- backend/start.sh
+++ backend/start.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-# Criar o ambiente virtual se ainda não existir
-if [ ! -d "venv" ]; then
-    python3 -m venv venv
-fi
-
-# Ativar o ambiente virtual
-source venv/bin/activate
-
-# Atualizar pip
-pip install --upgrade pip
-
-# Instalar as dependências
-pip install -r requirements.txt
 
 # Executar o servidor Flask
 exec python app.py
